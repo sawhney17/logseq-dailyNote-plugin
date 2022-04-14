1ed3552bdd21528bd305105fd1fe9390f2099fe5
sawhney17 <80150109+sawhney17@users.noreply.github.com>
2022-04-14 17:41:00 +0400

    DEbug code

--- index.ts
+++ index.ts
@@ -21,11 +21,11 @@ const main = async () => {
       key: "goToDailyNote",
       label: "Go to the daily note page for today",
       keybinding: {
-        mode: "global",
         binding: logseq.settings.keyboardShortcut
       },
     }, async () => {
       //get yesterdays date as a date object
+      logseq.App.showMsg("Daily note page toggled")
       let date = new Date();
       date.setDate(date.getDate() - 1);
       logseq.App.pushState('page', { name: getDateForPage(date, (await logseq.App.getUserConfigs()).preferredDateFormat).slice(2, -2) })
